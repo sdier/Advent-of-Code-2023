f9130e1d3c4d41e6a0e0bab9305e4a5521b31625
Scott Dier <scott@dier.name>
2023-12-02 02:27:17 +0100

    Finish day 1.

--- Day 1/main.swift
+++ Day 1/main.swift
@@ -8,32 +8,67 @@
 import Foundation
 
 let input = """
-""".components(separatedBy: "\n").map({Array(String($0))})
+""".components(separatedBy: "\n")
+
+var numMap : [Character : [(numStr: String, numChar: Character)]] = [
+    "o": [("one", "1")],
+    "t": [("two", "2"), ("three", "3")],
+    "f": [("four", "4"), ("five", "5")],
+    "s": [("six", "6"), ("seven", "7")],
+    "e": [("eight", "8")],
+    "n": [("nine", "9")],
+]
 
 var calNums : [Int] = []
 
-for chars in input {
+for line in input {
+    print ("next line", line)
+    let chars = Array(line)
     var firstSeen = false
-    var firstNum : Character = " "
-    var lastNum : Character = " "
-    for char in chars {
-        if char.isWholeNumber {
-            if !firstSeen {
-                firstNum = char
-                firstSeen = true
+    var firstNum : Character? = nil
+    var lastNum : Character? = nil
+    var pos = 0
+    while pos < chars.count {
+        print("pos",pos,"char",chars[pos])
+        var number : Character? = nil
+    charSwitch: switch chars[pos] {
+        case let thisChar where thisChar.isWholeNumber:
+            print("is number", thisChar)
+            number = thisChar
+            pos += 1
+        case let thisChar where numMap.keys.contains(thisChar):
+            for numWord in numMap[thisChar]! {
+                print("check word", numWord.numStr)
+                if numWord.numStr.count <= chars.count - pos && String(chars[pos..<pos+numWord.numStr.count]) == numWord.numStr {
+                    print("found word", numWord.numChar)
+                    number = numWord.numChar
+                    pos += 1
+                    break charSwitch
+                }
             }
-            lastNum = char
+            pos += 1
+            continue
+        default:
+            print("add 1")
+            pos += 1
+            continue
         }
+        if !firstSeen {
+            firstNum = number!
+            firstSeen = true
+        }
+        lastNum = number!
     }
     var number = ""
-    number.append(firstNum)
-    number.append(lastNum)
+    number.append(firstNum!)
+    number.append(lastNum!)
     calNums.append(Int(number)!)
 }
 
 var calibration = 0
 
 for calNum in calNums {
+    print(calNum)
     calibration = calibration + calNum
 }
 
