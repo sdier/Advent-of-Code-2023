55bab6c2e6d337a7c0fdb4f8f0eebfeef8191013
Scott Dier <scott@dier.name>
2023-12-09 19:09:37 +0100

    Day 8 Complete!

--- Day 8/main.swift
+++ Day 8/main.swift
@@ -6,13 +6,19 @@
 //
 
 import Foundation
+import Euler
 
 typealias Location = (left: String, right: String)
 
 let map : [String: Location] = """
-AAA = (BBB, BBB)
-BBB = (AAA, ZZZ)
-ZZZ = (ZZZ, ZZZ)
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
 """.components(separatedBy: .newlines).reduce(into: [String: Location]()) {
     let x = $1.components(separatedBy: " = ")
     let y = x[1].components(separatedBy: ", ")
@@ -21,32 +27,38 @@ ZZZ = (ZZZ, ZZZ)
 }
 
 let directions : [Character] = Array("""
-LLR
+LR
 """)
 
 print(map)
 print(directions)
 
-var currentLocation = "AAA"
-let goalLocation = "ZZZ"
-var directionLoc = 0
-var steps = 0
+var currentLocations = Array(map.filter({$0.key.suffix(1) == "A"}).keys)
+var goalLocations = Array(map.filter({$0.key.suffix(1) == "Z"}).keys)
+var steps = Array(repeating: 0, count: currentLocations.count)
 
-while currentLocation != goalLocation {
-    switch directions[directionLoc] {
-    case "R":
-        currentLocation = map[currentLocation]!.right
-    case "L":
-        currentLocation = map[currentLocation]!.left
-    default:
-        fatalError()
-    }
-    steps += 1
-    directionLoc += 1
-    if directionLoc == directions.count {
-        directionLoc = 0
+print(currentLocations)
+print(goalLocations)
+
+for (idx, initialLocation) in currentLocations.enumerated() {
+    var directionLoc = 0
+    var currentLocation = initialLocation
+    while !goalLocations.contains(currentLocation) {
+        switch directions[directionLoc] {
+        case "R":
+            currentLocation = map[currentLocation]!.right
+        case "L":
+            currentLocation = map[currentLocation]!.left
+        default:
+            fatalError()
+        }
+        steps[idx] += 1
+        directionLoc += 1
+        if directionLoc == directions.count {
+            directionLoc = 0
+        }
     }
 }
 
 print(steps)
-
+try print(Tables().LCM(array: steps.map({BigInt($0)})))
