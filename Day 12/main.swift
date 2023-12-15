e8d098cf811e1ddeba76f6c44b13928a81ae8a00
Scott Dier <scott@dier.name>
2023-12-15 01:12:47 +0100

    Day 12 Complete!

--- Day 12/main.swift
+++ Day 12/main.swift
@@ -18,68 +18,106 @@ let input = """
     .map({$0.components(separatedBy: .whitespaces)})
     .map({(springs: Array($0[0]), count: $0[1].components(separatedBy: ",").map({Int($0)!}))})
 
-print(input)
+var cache : [String: Int] = [:]
 
-func genAndFilterSprings(springs: [Character], count: [Int]) -> Int {
-    // generate strings
-    for (idx, char) in springs.enumerated() {
-        var p = 0
-        if char == "?" {
-            var springs1 = springs
-            springs1[idx] = "."
-            p += genAndFilterSprings(springs: springs1, count: count)
-            var springs2 = springs
-            springs2[idx] = "#"
-            p += genAndFilterSprings(springs: springs2, count: count)
-            return p
-        }
-    }
-    // if no generation occured, then check and return result
+var calls = 0
+
+func genAndFilterSprings(springs: [Character], count: [Int], currentCount: Int?, index: Int?, countStack: [Int]?) -> Int {
     var curCount = 0
-    var countStack = Array(count.reversed())
-    for char in springs {
+    calls += 1
+
+    // rebuild state from previous checks
+    if currentCount != nil {
+        curCount = currentCount!
+    }
+    var cStack: [Int] = []
+    if countStack != nil {
+        cStack = countStack!
+    } else {
+        cStack = Array(count.reversed())
+    }
+    var cSprings = springs
+    if index != nil {
+        // chop off front of array
+        cSprings = Array(springs[index!...])
+    }
+
+    for (idx, char) in cSprings.enumerated() {
         switch char {
         case "#":
+            if cStack.isEmpty || curCount > cStack.last! {
+                // print("too many", cSprings.reduce(into: "") {$0 += String($1)}, cStack)
+                return 0
+            }
             curCount += 1
         case ".":
             if curCount > 0 {
-                if countStack.isEmpty {
-                    // print("empty", springs.reduce(into: "") {$0 += String($1)}, count)
+                if cStack.isEmpty {
+                    // print("empty", cSprings.reduce(into: "") {$0 += String($1)}, count)
                     return 0
                 }
-                if curCount == countStack.last {
+                if curCount == cStack.last {
                     curCount = 0
-                    _ = countStack.popLast()
+                    _ = cStack.popLast()
                 } else {
-                    // print("didn't match", springs.reduce(into: "") {$0 += String($1)}, count)
+                    //print("didn't match", cSprings.reduce(into: "") {$0 += String($1)}, cStack)
                     return 0
                 }
             }
+        case "?":
+            let pCacheKey = cSprings.reduce(into: "", {$0 += String($1)}) + "/" + cStack.reduce(into: "", {$0 += String($1) + ","}) + "/" + String(curCount)
+            if cache.keys.contains(pCacheKey) {
+                return cache[pCacheKey]!
+            }
+
+            var p = 0
+            var springs1 = cSprings
+            springs1[idx] = "."
+            p += genAndFilterSprings(springs: springs1, count: count, currentCount: curCount, index: idx, countStack: cStack)
+
+            var springs2 = cSprings
+            springs2[idx] = "#"
+            p += genAndFilterSprings(springs: springs2, count: count, currentCount: curCount, index: idx, countStack: cStack)
+
+            cache[pCacheKey] = p
+            return p
         default:
             fatalError("unexpected character")
         }
     }
     if curCount > 0 {
-        if countStack.isEmpty {
-            // print("final empty", springs.reduce(into: "") {$0 += String($1)}, count)
+        if cStack.isEmpty {
+            // print("final empty", cSprings.reduce(into: "") {$0 += String($1)}, count)
             return 0
         }
-        if curCount == countStack.last {
-            _ = countStack.popLast()
+        if curCount == cStack.last {
+            _ = cStack.popLast()
         }
     }
-    if countStack.isEmpty {
+    if cStack.isEmpty {
         return 1
     }
-    // print("reached end", springs.reduce(into: "") {$0 += String($1)}, count)
+    // print("reached end", cSprings.reduce(into: "") {$0 += String($1)}, count)
     return 0
 }
 
 // for each line in input
 var permutations = 0
 for line in input {
-    var p = genAndFilterSprings(springs: line.springs, count: line.count)
-    print(p, line.springs.reduce(into: "") {$0 += String($1)}, line.count)
+    var springs : [Character] = []
+    var count : [Int] = []
+    var p = 0
+    let times = 5
+    for _ in 0..<(times-1) {
+        springs.append(contentsOf: line.springs)
+        springs.append("?")
+        count.append(contentsOf: line.count)
+    }
+    springs.append(contentsOf: line.springs)
+    count.append(contentsOf: line.count)
+    p = genAndFilterSprings(springs: springs, count: count, currentCount: nil, index: nil, countStack: nil)
+    print(calls)
+    calls = 0
     permutations += p
 }
 
